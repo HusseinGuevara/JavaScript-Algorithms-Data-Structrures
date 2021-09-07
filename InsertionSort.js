0d20915c32cf9f8bd84e509670f4ad0027f7d0d9
Hussein Guevara <80933892+HusseinGuevara@users.noreply.github.com>
2021-09-07 09:29:14 -0700

    Add files via upload

---
+++ InsertionSort.js
@@ -0,0 +1,15 @@
+const arr1 = [6,5,3,2,1,4]
+const arr2 = [11254,51,100,1,888,99999]
+
+function insertionSort(arr) {
+    for(var i = 1; i < arr1.length; i++) {
+        var currentValue = arr[i];
+        for(var j = i - 1; j >= 0 && arr[j] > currentValue; j--) {
+            arr[j + 1] = arr[j];
+            arr[j] = currentValue;
+        }
+        console.log("Inner Loop: " + arr)
+    }
+}
+insertionSort(arr1);
+insertionSort(arr2);
